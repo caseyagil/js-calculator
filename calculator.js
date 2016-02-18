62eed8099578b855b9c8680020d6cb77e7951cc6
caseyagil <caseyagil@gmail.com>
2016-02-18 07:51:47 -0500

    homework turn in

---
+++ calculator.js
@@ -0,0 +1,23 @@
+var calc = {
+   add: function (a, b) {
+ console.log(a + b); /* to test if it runs properly*/
+     return a + b;
+   } ,
+
+   sub: function (a, b) {
+     return a - b;
+   } ,
+   divd: function(a, b) {
+     return a / b;
+   } ,
+   mul: function(a, b) {
+     return a * b;
+   } ,
+ };
+
+ /* various calculations */
+
+ calc.add();
+ calc.sub();
+ calc.divd();
+ calc.mul();
